8a0aa5a7d87f1473c757848c746e54ee0f4d5d19
naushinhoque <fnmh@yahoo.com>
2023-12-08 15:09:55 -0600

    checkpayload

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,9 +1,37 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Car.getById(req.params.id)
+    if (!car) {
+      next({ status: 40, message: 'not found' })
+    } else {
+      req.car = car
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (!req.body.vin) return next({ 
+    status: 400, 
+    message: 'vin is missing'
+  })
+  if (!req.body.make) return next({ 
+    status: 400, 
+    message: 'make is missing'
+  })
+  if (!req.body.model) return next({ 
+    status: 400, 
+    message: 'model is missing'
+  })
+  if (!req.body.mileage) return next({ 
+    status: 400, 
+    message: 'mileage is missing'
+  })
+  next()
 }
 
 const checkVinNumberValid = (req, res, next) => {
@@ -12,4 +40,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,25 @@
 // DO YOUR MAGIC
+const express = require('express')
+const Car = require('./cars-model')
+const { checkCarId, checkVinNumberUnique, checkCarPayload, checkVinNumberValid } = require('./cars-middleware')
+
+const router = express.Router()
+
+router.get('/', checkCarId async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
+})
+
+router.post('/', checkCarPayload, checkVinNumberUnique, checkVinNumberValid, async (req, res, next) => {
+    res.json('posting new car')
+})
+
+module.exports = router
